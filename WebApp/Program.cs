35ede78952caf99dc9799f05d3e1390d59171201
Amir Diyanatdar <159172919+Amir-Diyanatdar@users.noreply.github.com>
2025-10-19 10:48:22 +0330

    Day001

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync($"The method is: {context.Request.Method} \r \n");
+    await context.Response.WriteAsync($"The url is: {context.Request.Path} \r \n");
+    await context.Response.WriteAsync($"The query string is: {context.Request.QueryString} \r \n");
+    await context.Response.WriteAsync($"The headers are: \r \n");
+    foreach (var key in context.Request.Headers.Keys)
+    {
+        await context.Response.WriteAsync($"{key}: {context.Request.Headers[key]} \r \n");
+    }
+
+});
 
 app.Run();
